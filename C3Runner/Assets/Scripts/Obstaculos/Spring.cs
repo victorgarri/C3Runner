5a632d5a4ae7516fd04d433eb32933c9975cbdbc
Nekupaska <dan-rex@hotmail.es>
2023-03-05 17:55:13 +0100

    Spring nuevo con animación
    Se deja el modelo antiguo oculto de todas formas

--- C3Runner/Assets/Scripts/Obstaculos/Spring.cs
+++ C3Runner/Assets/Scripts/Obstaculos/Spring.cs
@@ -6,11 +6,29 @@ public class Spring : MonoBehaviour
 {
     public float force = 10;
     Transform direction;
+    public Animator baseModelAnim;
+    Animator mainAnim;
+    float lengthBoing = 1;
 
     void Start()
     {
+        mainAnim = GetComponent<Animator>();
         direction = transform.Find("Direction");
         force *= GameManager.gravityScale;
+
+
+        //ASIGNACIÓN DINÁMICA DE LA LONGITUD DE LA ANIMACIÓN
+        AnimationClip[] clips = baseModelAnim.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        {
+            switch (clip.name)
+            {
+                case "Boing":
+                    lengthBoing = clip.length;
+                    break;
+            }
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,6 +39,15 @@ public class Spring : MonoBehaviour
         {
             obj.GetComponent<Rigidbody>().velocity = Vector3.zero; //Reset velocity
             obj.GetComponent<Rigidbody>().AddForce(direction.up * force, ForceMode.Impulse);
+            baseModelAnim.Play("Boing");
+            StartCoroutine("PauseMainAnimator");
         }
+    }
+
+    IEnumerator PauseMainAnimator()
+    {
+        mainAnim.speed = 0;
+        yield return new WaitForSeconds(lengthBoing); //Creo que la animación del spring es de un segundo solo
+        mainAnim.speed = 1;
     }
 }
