deee20293050b5bd83af65a1b66f69a759594d9d
Nekupaska <dan-rex@hotmail.es>
2023-05-08 17:17:44 +0200

    Cap framerate

---
+++ C3Runner/Assets/Scripts/Otros/CapFramerate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapFramerate : MonoBehaviour
+{
+    public int vsync = 0;
+    public int fps = 60;
+
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = vsync;  // VSync must be disabled
+        Application.targetFrameRate = fps;
+    }
+
+    void Update()
+    {
+        //QualitySettings.vSyncCount = vsync;  // VSync must be disabled
+        //Application.targetFrameRate = fps;
+    }
+}
