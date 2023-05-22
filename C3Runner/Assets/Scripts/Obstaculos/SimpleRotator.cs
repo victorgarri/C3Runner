d1037a2c2c10e03dd2dbe21596e3e80069693c3f
DanielColmenaMartos <73236597+DanielColmenaMartos@users.noreply.github.com>
2023-05-22 13:58:04 +0200

    Optimización
    Quitado animators de algunas cosas raras
    SimpleRotator editado, queda poner a true/false en escena los que nos interesen.

--- C3Runner/Assets/Scripts/Obstaculos/SimpleRotator.cs
+++ C3Runner/Assets/Scripts/Obstaculos/SimpleRotator.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class SimpleRotator : MonoBehaviour
 {
     public float rotationAmount = 1;
+    public bool forceActive = false;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
     private void FixedUpdate()
     {
-        transform.Rotate(new Vector3(0, rotationAmount, 0));
+        cam = Camera.main;
+        if ((forceActive) || (cam != null && Vector3.Distance(transform.position, cam.transform.position) < 400))
+            transform.Rotate(new Vector3(0, rotationAmount, 0));
     }
 }
