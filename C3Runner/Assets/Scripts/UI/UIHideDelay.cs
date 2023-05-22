4250b16671d5f2a61f8671a9877cee5b2049a779
DanielColmenaMartos <73236597+DanielColmenaMartos@users.noreply.github.com>
2023-05-22 13:23:34 +0200

    Controles deshabilitados al principio 3D
    Cuando está mostrando del canvas de los controles, playerInput desactivado

--- C3Runner/Assets/Scripts/UI/UIHideDelay.cs
+++ C3Runner/Assets/Scripts/UI/UIHideDelay.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIHideDelay : MonoBehaviour
 {
+    public PlayerInput pi;
 
     void Start()
     {
@@ -14,6 +16,7 @@ public class UIHideDelay : MonoBehaviour
     IEnumerator Hide()
     {
         yield return new WaitForSeconds(5);
+        pi.enabled = true;
         gameObject.SetActive(false);
     }
 }
