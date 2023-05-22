0d4b634621dd960daac2bd979d52fd693a1c054c
DanielColmenaMartos <73236597+DanielColmenaMartos@users.noreply.github.com>
2023-05-22 11:34:59 +0200

    Resolution settings Android

--- C3Runner/Assets/Scripts/Otros/CapFramerate.cs
+++ C3Runner/Assets/Scripts/Otros/CapFramerate.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CapFramerate : MonoBehaviour
 {
     public int vsync = 0;
     public int fps = 60;
-    public int resolutionX = 1920, resolutionY = 1080;
+    public int resolutionX = 1366, resolutionY = 768;
     public float renderDistance = 1500;
+    public bool active;
+    public static float factor = 1;
 
 
     void Awake()
     {
-        //ApplySettings();
+        if (active)
+            ApplySettings();
 
     }
 
@@ -24,12 +28,25 @@ public class CapFramerate : MonoBehaviour
 
     public void ApplySettings()
     {
-#if !(UNITY_ANDROID || UNITY_EDITOR)
-        //QualitySettings.vSyncCount = vsync;  // VSync must be disabled
-        Application.targetFrameRate = fps;
+#if (UNITY_ANDROID || UNITY_EDITOR)
+
+        factor = 1;
+        if (Screen.height > resolutionY || Screen.width > Screen.width)
+        {
+            factor = ((float)resolutionY / (float)Screen.height);
+        }
+        int finalResX = (int)(Screen.width * factor);
+        int finalResY = (int)(Screen.height * factor);
+        if (finalResX % 2 != 0) { finalResX++; }
+        if (finalResY % 2 != 0) { finalResY++; }
+
+        Screen.SetResolution(finalResX, finalResY, FullScreenMode.ExclusiveFullScreen, 60);
+        print("Screen:" + Screen.width + "x" + Screen.height + "; Factor: " + factor);
+
 
 #else
-        Screen.SetResolution(resolutionX, resolutionY, FullScreenMode.ExclusiveFullScreen, 60);
+        //QualitySettings.vSyncCount = vsync;  // VSync must be disabled
+        Application.targetFrameRate = fps;
 #endif
     }
 }
